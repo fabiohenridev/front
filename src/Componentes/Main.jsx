1baae4132a3f620c0364ed751135b3a31801f3cc
fabio henrique <henri8274@gmail.com>
2025-01-30 18:54:54 -0300

    update

--- src/Componentes/Main.jsx
+++ src/Componentes/Main.jsx
@@ -3,62 +3,55 @@ import './Main.css';
 import Mensagens from './Mensagens';
 
 export default function Main() {
-
     const [nome, setNome] = useState('');
     const [texto, setTexto] = useState('');
     const [mensagem, setMensagem] = useState('');
 
     const EnviarDados = (e) => {
-
         e.preventDefault();
 
-        const Dados = { nome, texto }
+        const Dados = { nome, texto };
 
         fetch('https://7761-186-236-211-59.ngrok-free.app/Dados', {
-
             method: 'POST',
             headers: {
-                'Content-type': 'application/json',
+                'Content-Type': 'application/json',
                 'ngrok-skip-browser-warning': 'true',
+                'User-Agent': 'CustomUserAgent', // Adiciona o cabeçalho de User-Agent customizado
             },
-
-            body: JSON.stringify(Dados)
-
+            body: JSON.stringify(Dados),
         })
-          
-             .then(()=>{
-               console.log("Mensagem enviada")
-               setTexto('');
-             })
-
-            .catch((erro) => {
-                console.log("erro ao enviar dados", erro)
+            .then(() => {
+                console.log("Mensagem enviada");
+                setTexto('');
             })
-
+            .catch((erro) => {
+                console.log("Erro ao enviar dados", erro);
+            });
     }
 
     return (
         <div>
-            <Mensagens/>
+            <Mensagens />
             <main className="Main">
-                <div className='Buttons'>
-                    <div className='BotoesMensagens'>
-                        <input type='text'
-                            placeholder='Nome obrigatório'
+                <div className="Buttons">
+                    <div className="BotoesMensagens">
+                        <input type="text"
+                            placeholder="Nome obrigatório"
                             value={nome}
                             onChange={(e) => setNome(e.target.value)}
-                        ></input>
-                        <div className='Enviar'>
-                            <input type='email'
+                        />
+                        <div className="Enviar">
+                            <input type="email"
                                 value={texto}
-                                placeholder='Mensagem'
+                                placeholder="Mensagem"
                                 onChange={(e) => setTexto(e.target.value)}
-                            ></input>
-                            <button onClick={EnviarDados}><i class="bi bi-send-fill BotaoEnviar"></i></button>
+                            />
+                            <button onClick={EnviarDados}><i className="bi bi-send-fill BotaoEnviar"></i></button>
                         </div>
                     </div>
                 </div>
             </main>
         </div>
-    )
+    );
 }
