3fe7a5e842becf775cfd09ba8aa81a06c690e1b5
fabio henrique <henri8274@gmail.com>
2025-01-30 20:09:31 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import './App.css'
 import Header from './Componentes/Header'
 import Main from './Componentes/Main'
-import Footer from './Componentes/Footer'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,7 +12,7 @@ function App() {
    <div>
      <Header/>
      <Main/>
-     <Footer/>
+   
    </div>
   )
 }
